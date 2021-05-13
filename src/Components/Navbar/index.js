91e841ce8a6f104b4fac69ae8b9bcfd173c60305
Thais Quintela <thais.s.quintela@gmail.com>
2021-05-13 18:59:52 -0300

    Add details

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -4,7 +4,7 @@ import './styles.css';
 
 function Navbar() {
   return (
-    <nav className="navbar fixed-top navbar-light d-flex justify-content-end">
+    <nav className="navbar fixed-top navbar-light d-flex justify-content-end pe-3">
       <a href="#home" className="navbar-brand mx-2 text-white"><span>Home</span></a>
       <a href="#about" className="navbar-brand mx-2 text-white"><span>About</span></a>
       <a href="#portfolio" className="navbar-brand mx-2 text-white"><span>Portfolio</span></a>
